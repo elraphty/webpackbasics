35de04e6a187662572ac7bcf2d31a8101a990934
elraphty <elraphtyofficial@gmail.com>
2019-02-12 17:17:45 +0100

    added styles

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-
+require('!style-loader!css-loader!./styles.css');
   function component() {
     var element = document.createElement('div');
    // Lodash, currently included via a script, is required for this line to work
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,5 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  modules: {
-      
   }
 };
