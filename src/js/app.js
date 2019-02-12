6e343fab3ba6ef7a3ac75ee57bd3c88b4eff5cca
elraphty <elraphtyofficial@gmail.com>
2019-02-12 18:24:06 +0100

    added webpack-dev-server

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,15 @@ require('../styles/styles.css');
    // Lodash, currently included via a script, is required for this line to work
   // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    
     return element;
   }
+
+  function component1 (params) {
+    var element1 = document.createElement('h1');
+    element1.innerHTML = 'h1 from webpack';
+    return element1;
+  }
+  
   document.body.appendChild(component());
+  document.body.appendChild(component1());
